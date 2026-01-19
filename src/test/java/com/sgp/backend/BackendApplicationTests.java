5b182d2a0b05928a907a8c34d9e2b5f83369cb70
Francisco <villalbaportillaf.fv@gmail.com>
2026-01-19 15:28:03 -0500

    primer avance guia

---
+++ src/test/java/com/sgp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
